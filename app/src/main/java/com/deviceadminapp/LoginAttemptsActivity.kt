9f0fd6d74851c6a0f1b0f31a8a2b5379f1a115fa
Achal-H14 <apcomp1410@gmail.com>
2021-12-15 18:18:24 +0530

    Created custome base adapter and fake attempts

--- app/src/main/java/com/deviceadminapp/LoginAttemptsActivity.kt
+++ app/src/main/java/com/deviceadminapp/LoginAttemptsActivity.kt
@@ -1,11 +1,53 @@
 package com.deviceadminapp
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
+import com.deviceadminapp.adapters.LoginAtteptAdapter
+import com.deviceadminapp.models.LoginAttemptModel
+import com.deviceadminapp.receivers.DevAdminReceiver
 
 class LoginAttemptsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_attempts)
+
+        supportActionBar?.hide()
+
+        val componentName = ComponentName(this, DevAdminReceiver::class.java)
+        val policyManager = getSystemService(DEVICE_POLICY_SERVICE) as DevicePolicyManager
+
+        if (policyManager.isAdminActive(componentName)) {
+            Log.v("$$$$$$", "Admin is already activated")
+
+
+            LoginAttemptModel.addAttempt(LoginAttemptModel(true))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(true))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(false))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(true))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(false))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(true))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(false))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(true))
+            LoginAttemptModel.addAttempt(LoginAttemptModel(false))
+
+
+
+            val lvLoginAttempts = findViewById<ListView>(R.id.lv_login_attempts)
+            val loginAttemptsAdapter = LoginAtteptAdapter(this, LoginAttemptModel.listOfAttempts)
+            lvLoginAttempts.adapter = loginAttemptsAdapter
+
+        } else {
+            // get dev admin permission
+            val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, componentName)
+            intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                getString(R.string.device_admin_explanation))
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/deviceadminapp/adapters/LoginAtteptAdapter.kt
@@ -0,0 +1,49 @@
+package com.deviceadminapp.adapters
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import androidx.annotation.RequiresApi
+import com.deviceadminapp.R
+import com.deviceadminapp.models.LoginAttemptModel
+
+
+class LoginAtteptAdapter(private val context : Context, private val dataSource : ArrayList<LoginAttemptModel>) : BaseAdapter() {
+
+
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+
+    override fun getItem(p0: Int): LoginAttemptModel {
+        return  dataSource[p0]
+    }
+
+    override fun getItemId(p0: Int): Long {
+        return getItem(p0).id
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("ViewHolder")
+    override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val attemptCard = inflater.inflate(R.layout.login_attempt_card, p2, false)
+
+        val attemptStatus = attemptCard.findViewById<View>(R.id.v_success)
+        val attemptTime = attemptCard.findViewById<TextView>(R.id.tv_attempt_time)
+
+        val attempt = getItem(p0)
+
+        attemptTime.text = attempt.attemptTime
+        if (!attempt.success) attemptStatus.backgroundTintList = context.resources.getColorStateList(R.color.red)
+
+        return attemptCard
+
+    }
+}
